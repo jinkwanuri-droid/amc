131eb0b4cc44bf1570990b636f15876fff214938
JK_DROID <jinkwanuri@gmail.com>
2026-04-29 19:36:48 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useEffect } from 'react'
 import { useCalendarStore } from '@/store/useCalendarStore'
 import Header from '@/components/Header'
 import MonthView from '@/components/MonthView'
@@ -7,18 +8,19 @@ import ReservationModal from '@/components/ReservationModal'
 import SettingsModal from '@/components/SettingsModal'
 
 export default function Home() {
-  const { viewMode, isResModalOpen, isSetModalOpen } = useCalendarStore();
+  const { viewMode, isResModalOpen, isSetModalOpen, fetchReservations } = useCalendarStore();
+
+  // 화면이 켜지면 DB에서 예약 데이터를 가져옵니다!
+  useEffect(() => {
+    fetchReservations();
+  }, [fetchReservations]);
 
   return (
     <main className="h-screen flex flex-col p-6 max-w-7xl mx-auto overflow-hidden">
       <Header />
-      
-      {/* 캘린더 영역 (카드섹션 안에서만 스크롤되도록 설정) */}
       <div className="flex-1 bg-white/70 backdrop-blur-xl border border-white/40 shadow-xl rounded-2xl overflow-hidden flex flex-col">
         {viewMode === 'month' ? <MonthView /> : <WeekView />}
       </div>
-
-      {/* 모달창 렌더링 */}
       {isResModalOpen && <ReservationModal />}
       {isSetModalOpen && <SettingsModal />}
     </main>
